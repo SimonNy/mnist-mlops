552dcb046d90394e13ac8ecd2e6d40f898a5ceea
SimonNy <s_nyrup@protonmail.com>
2024-05-16 15:19:04 +0200

    init processing of raw data

--- mnist_mlops/data/make_dataset.py
+++ mnist_mlops/data/make_dataset.py
@@ -1,3 +1,33 @@
-if __name__ == '__main__':
-    # Get the data and process it
-    pass
+import click
+import torch
+
+
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    """Normalize images."""
+    return (images - images.mean()) / images.std
+
+@click.command()
+@click.option("raw_dir", default="data/raw", help="Path to row data directory")
+@click.option("processed_dir", default="data/processed", help="Path to processed data directory")
+
+def make_data(raw_dir: str, processed_dir: str):
+    """Process raw data and save it to processed directory"""
+    train_images, train_target = [], []
+    for i in range(5):
+        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
+        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
+    train_images = torch.cat(train_images)
+    train_target = torch.cat(train_target)
+
+    test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
+    test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
+
+    train_images = train_images.unsqueeze(1).float()
+    test_images = test_images.unsqueeze(1).float()
+    train_target = train_target.long()
+    test_target = test_target.long()
+
+    torch.save(train_images, f"{processed_dir}/train_images.pt")    
+    torch.save(train_target, f"{processed_dir}/train_target.pt")
+    torch.save(test_images, f"{processed_dir}/test_images.pt")
+    torch.save(test_target, f"{processed_dir}/test_target.pt")

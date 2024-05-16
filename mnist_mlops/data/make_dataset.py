61a28b73eda3b9ae951b4d1e1a94d91a06390f3b
SimonNy <s_nyrup@protonmail.com>
2024-05-16 22:36:59 +0200

    update joined paths with os.path.join

--- mnist_mlops/data/make_dataset.py
+++ mnist_mlops/data/make_dataset.py
@@ -1,3 +1,4 @@
+import os
 import click
 import torch
 
@@ -7,30 +8,30 @@ def normalize(images: torch.Tensor) -> torch.Tensor:
     return (images - images.mean()) / images.std
 
 @click.command()
-@click.option("--raw_dir", default="data/raw", help="Path to raw data directory")
-@click.option("--processed_dir", default="data/processed", help="Path to processed data directory")
+@click.option("--raw_dir", default=os.path.join("data","raw"), help="Path to raw data directory")
+@click.option("--processed_dir", default=os.path.join("data","processed"), help="Path to processed data directory")
 
 def make_data(raw_dir: str, processed_dir: str):
     """Process raw data and save it to processed directory"""
     train_images, train_target = [], []
     for i in range(5):
-        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
-        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
+        train_images.append(torch.load(os.path.join(raw_dir, f"train_images_{i}.pt")))
+        train_target.append(torch.load(os.path.join(raw_dir, f"train_target_{i}.pt")))
     train_images = torch.cat(train_images)
     train_target = torch.cat(train_target)
 
-    test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
-    test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
+    test_images: torch.Tensor = torch.load(os.path.join(raw_dir, "test_images.pt"))
+    test_target: torch.Tensor = torch.load(os.path.join(raw_dir,"test_target.pt"))
 
     train_images = train_images.unsqueeze(1).float()
     test_images = test_images.unsqueeze(1).float()
     train_target = train_target.long()
     test_target = test_target.long()
 
-    torch.save(train_images, f"{processed_dir}/train_images.pt")    
-    torch.save(train_target, f"{processed_dir}/train_target.pt")
-    torch.save(test_images, f"{processed_dir}/test_images.pt")
-    torch.save(test_target, f"{processed_dir}/test_target.pt")
+    torch.save(train_images, os.path.join(processed_dir, "train_images.pt"))
+    torch.save(train_target, os.path.join(processed_dir, "train_target.pt"))
+    torch.save(test_images, os.path.join(processed_dir, "test_images.pt"))
+    torch.save(test_target, os.path.join(processed_dir, "test_target.pt"))
 
 
 if __name__ == "__main__":
